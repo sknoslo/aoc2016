f8b32068e7d68b3e82356d2ddff34e78650fad07
Steffen Olson <sknoslo@gmail.com>
2024-12-18 19:47:15 -0500

    day01

--- day01/main.go
+++ day01/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"sknoslo/aoc2016/utils"
+	"sknoslo/aoc2016/vec2"
+	"strconv"
+	"strings"
 )
 
 var input string
@@ -16,9 +20,51 @@ func main() {
 }
 
 func partone() string {
-	return "incomplete"
+	dir := vec2.North
+	pos := vec2.New(0, 0)
+
+	for _, inst := range strings.Split(input, ", ") {
+		var d rune
+		var l int
+		fmt.Sscanf(inst, "%c%d", &d, &l)
+
+		if d == 'L' {
+			dir = dir.RotateCardinalCCW()
+		} else {
+			dir = dir.RotateCardinalCW()
+		}
+
+		pos = pos.Add(dir.Mul(l))
+	}
+
+	return strconv.Itoa(utils.Abs(pos.X) + utils.Abs(pos.Y))
 }
 
 func parttwo() string {
-	return "incomplete"
+	dir := vec2.North
+	pos := vec2.New(0, 0)
+	s := make(map[vec2.Vec2]bool, 256)
+
+mainloop:
+	for _, inst := range strings.Split(input, ", ") {
+		var d rune
+		var l int
+		fmt.Sscanf(inst, "%c%d", &d, &l)
+
+		if d == 'L' {
+			dir = dir.RotateCardinalCCW()
+		} else {
+			dir = dir.RotateCardinalCW()
+		}
+
+		for range l {
+			pos = pos.Add(dir)
+			if s[pos] {
+				break mainloop
+			}
+			s[pos] = true
+		}
+	}
+
+	return strconv.Itoa(utils.Abs(pos.X) + utils.Abs(pos.Y))
 }
